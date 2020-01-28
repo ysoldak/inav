2094e9870bf68eb8bc9a509fe6c5057b156b249f
Yurii Soldak <ysoldak@gmail.com>
2020-01-28 15:57:51 +0100

    Enable servo on S5

--- src/main/target/MATEKF405/target.c
+++ src/main/target/MATEKF405/target.c
@@ -33,7 +33,7 @@ const timerHardware_t timerHardware[] = {
     DEF_TIM(TIM8, CH3, PC8,  TIM_USE_MC_MOTOR |                    TIM_USE_FW_SERVO, 0, 1), // S3  UP(2,1)
     DEF_TIM(TIM8, CH4, PC9,  TIM_USE_MC_MOTOR |                    TIM_USE_FW_SERVO, 0, 0), // S4  UP(2,1)
 
-    DEF_TIM(TIM2, CH1, PA15, TIM_USE_MC_MOTOR | TIM_USE_LED                        , 0, 0), // S5  UP(1,7)
+    DEF_TIM(TIM2, CH1, PA15, TIM_USE_MC_MOTOR | TIM_USE_LED      | TIM_USE_FW_SERVO, 0, 0), // S5  UP(1,7)
 
 #ifdef MATEKF405_SERVOS6
     DEF_TIM(TIM1, CH1, PA8,  TIM_USE_MC_MOTOR | TIM_USE_MC_SERVO | TIM_USE_FW_SERVO, 0, 0), // S6  UP(2,5)
